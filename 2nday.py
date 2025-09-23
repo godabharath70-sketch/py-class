d61d8021282fec9e1995caefe70b9fbec9176b2a
godabharath70-sketch <godabharath2004@gmail.com>
2025-09-23 12:27:02 +0530

    2day

---
+++ 2nday.py
@@ -0,0 +1,42 @@
+_Name = "Bharath"
+_age = 19
+print(f"My name is this what {_Name} and my age is this what {_age} years youth boy")
+
+Data = {
+    "Product":"watch",
+    "price":2300,
+    "Discount":20,
+    "productisavilable":True,
+    "TotalAmount":23.500,
+}
+print(Data)
+
+
+_num_list = [1,2,3,4,5,6,7,8,9,10]
+_list_num = 4 in _num_list
+print(_list_num)
+
+_data = [2,4,7,1,7,9]
+big_data = 0 not in _data
+print(big_data)
+
+_name = "Hello world"
+_name1 = 'w' not in _name
+print(_name1)
+
+__key = "Bharath"
+__key2 = 'w' in __key
+print (__key2)
+
+My_list1 = [9,3,2,3,1,5]
+my_list2 = [3,4,2,2,1,1]
+print(id(My_list1))
+print(id(my_list2))
+
+my_set1 = {1,2,3,4,5,3,3}
+my_set2 = {6,6,7,8,9}
+print(my_set1.difference(my_set2))
+print(my_set1)
+print(my_set2)
+
+
