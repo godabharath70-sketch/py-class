40ebbeadfcd48bb08a684d2cdc6a37de9c555c2f
godabharath70-sketch <godabharath2004@gmail.com>
2025-10-08 17:15:55 +0530

    1sat day my pyton

---
+++ Clinte_1.py
@@ -0,0 +1,9 @@
+You_name = "bHarath"
+You_emial = "@ojeudgee"
+
+
+
+def namee(name,email):
+    print(f"My name is {name} and my email is this {email}")
+namee("Bharath","goda@1234t5")
+
---
+++ Filterfun.py
@@ -0,0 +1,38 @@
+# My_students = [
+# {"Name":"Bharath","Age":20},
+# {"Name":"Bharath","Age":29},
+# {"Name":"raju","Age":45},
+# {"Name":"Bharath","Age":56},    
+# ]
+# passing_daata = input("Search the skills:")
+# passing_daata1 = (list(filter(lambda s:s["Name"] == passing_daata,My_students)))
+# if passing_daata1:
+#     print("Matching the data")
+#     for s in passing_daata1:
+#       print(s["Name"],"*_*",s["Age"])
+# else:
+#     print("Not data is matching")
+    
+
+import math 
+print(math.sqrt(65))
+
+
+from math import sqrt
+print(sqrt(65))
+
+import math as t  
+print(t.sqrt(65))
+
+from Clinte_1 import You_name
+print(You_name)
+
+# import keyword
+# print(keyword.softkwlist)
+# print(help("modules"))
+
+
+import random
+print(random.random())
+
+print(random.randint(444,777))
